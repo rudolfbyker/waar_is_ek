48121b162b9f480deb9ef407cb8e9f4e3e08245b
dolf <rudolfbyker@gmail.com>
2016-12-30 16:20:49 +0200

    "Waar is ek?" shows where you are in the world, and is a nice example app to start from if you want to build a location-aware app.

---
+++ src/main/java/com/example/dolf/myfirstapplication/MainActivity.java
@@ -0,0 +1,248 @@
+package com.example.dolf.myfirstapplication;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+
+import android.content.pm.PackageManager;
+
+import java.text.SimpleDateFormat;
+
+public class MainActivity
+        extends AppCompatActivity
+        implements
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        ResultCallback<LocationSettingsResult>,
+        LocationListener {
+
+    private TextView latitudeText;
+    private TextView longitudeText;
+    private TextView statusText;
+    private TextView timeText;
+    private TextView accuracyText;
+    private GoogleApiClient mGoogleApiClient;
+    private Location location;
+    private LocationRequest mLocationRequest;
+
+    private SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+
+    public static final int REQUEST_CHECK_SETTINGS = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        latitudeText = (TextView) findViewById(R.id.latitude);
+        longitudeText = (TextView) findViewById(R.id.longitude);
+        timeText = (TextView) findViewById(R.id.time);
+        accuracyText = (TextView) findViewById(R.id.accuracy);
+        statusText = (TextView) findViewById(R.id.status);
+
+        statusUpdate("Creating activity…");
+
+        requestLocationPermissions();
+
+        // Set up GAPI client
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(
+                        this /* FragmentActivity */,
+                        this /* OnConnectionFailedListener */
+                )
+                .addConnectionCallbacks(this)
+                .addApi(LocationServices.API)
+                .build();
+
+        createLocationRequest();
+
+    }
+
+    private void requestLocationPermissions() {
+        statusUpdate("Requesting location permissions…");
+        ActivityCompat.requestPermissions(this, new String[]{
+                Manifest.permission.ACCESS_FINE_LOCATION,
+//                android.Manifest.permission.ACCESS_COARSE_LOCATION
+        }, 0);
+    }
+
+    private void createLocationRequest() {
+
+        statusUpdate("Creating location request…");
+
+        // Create the location request and set the parameters
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+
+        // add the location request
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(mLocationRequest);
+
+        // check whether the current location settings are satisfied
+        PendingResult<LocationSettingsResult> result = LocationServices.SettingsApi.checkLocationSettings(mGoogleApiClient, builder.build());
+        result.setResultCallback(this);
+
+    }
+
+
+    protected void onStart() {
+        statusUpdate("Activity started.");
+        statusUpdate("Connecting to GAPI client…");
+        mGoogleApiClient.connect();
+        super.onStart();
+    }
+
+
+    protected void onStop() {
+        statusUpdate("Activity stopped.");
+        statusUpdate("Disconnecting from GAPI client…");
+        mGoogleApiClient.disconnect();
+        super.onStop();
+    }
+
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        statusUpdate("Connected to GAPI client.");
+        requestLastKnownLocation();
+        startLocationUpdates();
+    }
+
+    private void requestLastKnownLocation() {
+        statusUpdate("Getting last known location…");
+        if (
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+//                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                ) {
+            requestLocationPermissions();
+            return;
+        }
+        location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (location == null) {
+            statusUpdate("Location not available.");
+        } else {
+            statusUpdate("Got last known location:\n" + location.toString());
+            updateUI();
+        }
+    }
+
+    private void updateUI() {
+        latitudeText.setText(String.valueOf(location.getLatitude()));
+        longitudeText.setText(String.valueOf(location.getLongitude()));
+        timeText.setText(sdf.format(location.getTime()));
+        accuracyText.setText(String.format("Accuracy [m] = %.2f", location.getAccuracy()));
+    }
+
+
+    protected void startLocationUpdates() {
+        statusUpdate("Starting location updates…");
+        if (
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+//                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                ) {
+            requestLocationPermissions();
+            return;
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+    }
+
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        statusUpdate("Connection to GAPI client suspended.");
+    }
+
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        statusUpdate("Connection to GAPI client failed:\n" + connectionResult.toString());
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        statusUpdate("Got permissions:");
+        for (int i=0; i<permissions.length; i++) {
+            statusUpdate(String.format("%s %s", permissions[i], (grantResults[i] == PackageManager.PERMISSION_GRANTED) ? "granted" : "denied"));
+        }
+    }
+
+
+    protected void statusUpdate(String msg) {
+        statusText.append(msg+"\n");
+    }
+
+
+    @Override
+    public void onResult(@NonNull LocationSettingsResult locationSettingsResult) {
+        statusUpdate("Got location settings result.");
+        final Status status = locationSettingsResult.getStatus();
+        switch (status.getStatusCode()) {
+            case LocationSettingsStatusCodes.SUCCESS:
+                statusUpdate("All location settings are satisfied.");
+                // TODO initialize location requests here.
+                break;
+            case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                statusUpdate("User is required to change location settings.");
+                try {
+                    // Show the dialog by calling startResolutionForResult(),
+                    // and check the result in onActivityResult().
+                    status.startResolutionForResult(this, REQUEST_CHECK_SETTINGS);
+                } catch (IntentSender.SendIntentException e) {
+                    statusUpdate(e.toString());
+                }
+                break;
+            case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                statusUpdate("Cannot change location settings.");
+                break;
+        }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        statusUpdate(String.format("Got activity result: %d, %d", requestCode, resultCode));
+        switch (requestCode) {
+            case REQUEST_CHECK_SETTINGS:
+                if (resultCode == RESULT_OK) {
+                    statusUpdate("Location settings have changed. Trying again.");
+                    createLocationRequest();
+                } else {
+                    statusUpdate("Failed to change location settings.");
+                    // TODO: complain that the app won't work without location settings
+                }
+                break;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+
+    @Override
+    public void onLocationChanged(Location location) {
+        statusUpdate("Got new location:\n" + location.toString());
+        this.location = location;
+        updateUI();
+    }
+
+
+}
