a893cba44979bf8b2cc7d43f5d03184dbbab9971
Rudolf Byker <rudolfbyker@gmail.com>
2018-07-24 15:55:00 +0200

    implementing location receiver

--- app/src/main/java/com/example/dolf/waarisek/MainActivity.java
+++ app/src/main/java/com/example/dolf/waarisek/MainActivity.java
@@ -1,13 +1,68 @@
 package com.example.dolf.waarisek;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MainActivity extends Activity {
+
+    public static final int REQ_GET_LAST_KNOWN_LOCATION = 12345;
+
+    private FusedLocationProviderClient mFusedLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
     }
+
+    private void getLastKnownLocation() {
+
+        toast("Asking for last known location...");
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            toast("Requesting permissions...");
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION
+            }, REQ_GET_LAST_KNOWN_LOCATION);
+            return;
+        }
+
+        mFusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+                            // Logic to handle location object
+                            toast("Got a location: " + location.toString());
+                        } else {
+                            toast("Location is null.");
+                        }
+                    }
+                });
+    }
+
+    private void toast(CharSequence text) {
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    private void statusUpdate(CharSequence text) {
+
+    }
+
 }
